ebaa1cc71f34b4110f01b547b96e2ef26a7473eb
Manik MT <165809938+ManikZellyo@users.noreply.github.com>
2024-04-13 17:32:44 +0600

    v6

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var swiper1 = new Swiper(".testiSwiper", {
   spaceBetween: 30,
   loop: true,
   autoplay: {
-    delay: 2500, 
+    delay: 2500,
     disableOnInteraction: true,
   },
   pagination: {
@@ -27,62 +27,64 @@ var swiper1 = new Swiper(".testiSwiper", {
       spaceBetween: 50,
     },
   },
-  
+
 });
 
-var swiper2 = new Swiper(".gallerySwiper", {
-  slidesPerView: 5,
-  spaceBetween: 10,
-  grid: {
-    rows: 2,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    loop: true,
-  },
+
+
+var swiper2 = new Swiper('.gallerySwiper', {
+  slidesPerView: 5, // Number of slides visible in the viewport
+  spaceBetween: 10, // Space between slides
+  loop: true, // Enable continuous loop
+  speed: 9000, // Transition speed in milliseconds
   autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  navigation: {
-    nextEl: ".cs-gallery-next",
-    prevEl: ".cs-gallery-prev",
+      delay: 0, // Delay before starting autoplay
+      disableOnInteraction: false, // Keep autoplay running even when user interacts
   },
   breakpoints: {
-    320: {
-      slidesPerView: 2,
-      grid: {
-        rows: 2,
+      320: {
+          slidesPerView: 2,
       },
-    },
-    768: {
-      slidesPerView: 3,
-      grid: {
-        rows: 2,
+      768: {
+          slidesPerView: 3,
+          spaceBetween: 10,
       },
-      spaceBetween: 10,
-    },
-    1200: {
-      slidesPerView: 5,
-      grid: {
-        rows: 2,
+      1200: {
+          slidesPerView: 5,
+          spaceBetween: 10,
       },
-      spaceBetween: 10,
-    },
   },
 });
 
-$.fn.parallax = function (resistance, mouse) {
-  $el = $(this);
-  TweenLite.to($el, 0.2, {
-    x: -((mouse.clientX - window.innerWidth / 2) / resistance),
-    y: -((mouse.clientY - window.innerHeight / 2) / resistance)
-  });
-};
 
-$(document).mousemove(function (e) {
-  $(".movein").parallax(10, e);
-  $(".cloud2").parallax(20, e);
-  $(".cloud3").parallax(30, e);
+var swiper3 = new Swiper('.gallerySwiper2', {
+  slidesPerView: 5.1, // Number of slides visible in the viewport
+  spaceBetween: 10, // Space between slides
+  loop: true, // Enable continuous loop
+  speed: 8500, // Transition speed in milliseconds
+  autoplay: {
+      delay: 0, // Delay before starting autoplay
+      disableOnInteraction: false, // Keep autoplay running even when user interacts
+  },
+  breakpoints: {
+      320: {
+          slidesPerView: 2.1,
+      },
+      768: {
+          slidesPerView: 3.1,
+          spaceBetween: 10,
+      },
+      1200: {
+          slidesPerView: 5.1,
+          spaceBetween: 10,
+      },
+  },
 });
+// Listen for Swiper's 'transitionEnd' event to handle infinite loop
+swiper.on('transitionEnd', function () {
+  if (swiper.realIndex === swiper.slides.length - 1) {
+      swiper.slideTo(swiper.params.slidesPerView - 1, 0, false);
+  }
+});
+
+swiper.autoplay.start();
